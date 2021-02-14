1b22f471e794c93ba522557595ae8ff27c60d519
linvonavi <74917456+linvonavi@users.noreply.github.com>
2021-02-14 18:29:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -714,7 +714,7 @@ def getAngleBetweenPoints(x_orig, y_orig, x_landmark, y_landmark):
     return angle_trunc(math.atan2(deltaY, deltaX))
 
 def angle(cam, cam_a, i):
-    if 45 <(math.degrees(getAngleBetweenPoints(cam[0], cam[1], i[0], i[1]))+cam_a[0]+360)%360 < 315:
+    if 90 <(math.degrees(getAngleBetweenPoints(cam[0], cam[1], i[0], i[1]))+cam_a[0]+360)%360 < 270:
         return 1000, 1000
     i = [i[0]-cam[0], i[1]-cam[1], i[2]-cam[2]]
     c1 = math.cos(math.radians(cam_a[0]))
